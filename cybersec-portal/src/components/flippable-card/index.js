ae00feebf93984545ea019c2e90ddafbff268daa
Gaurav <harishtheballer@gmail.com>
2023-07-03 22:01:58 +0530

    flipcards

---
+++ cybersec-portal/src/components/flippable-card/index.js
@@ -0,0 +1,14 @@
+import './index.scss'
+
+import React from 'react'
+
+const flipcard = () => {
+  return (
+    <div className='flippable-card-container'>
+        
+      
+    </div>
+  )
+}
+
+export default flipcard
